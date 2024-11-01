bf98af2edf86a7bb999fa9361b9c9a0b630ae91a
xy9288 <wang63209chen328@163.com>
2024-11-01 14:33:38 +0800

    update(exception):异常统一处理

--- datalink-web/src/main/java/com/leon/datalink/web/exception/DatalinkExceptionHandler.java
+++ datalink-web/src/main/java/com/leon/datalink/web/exception/DatalinkExceptionHandler.java
@@ -3,36 +3,29 @@
 package com.leon.datalink.web.exception;
 
 import com.leon.datalink.core.utils.ExceptionUtil;
+import com.leon.datalink.core.utils.Loggers;
+import com.leon.datalink.web.model.RestResult;
+import com.leon.datalink.web.model.RestResultUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * Exception handler for console module.
+ * Exception handler for web module.
  *
  * @author Leon
  */
 @ControllerAdvice
 public class DatalinkExceptionHandler {
-    
-    private static final Logger LOGGER = LoggerFactory.getLogger(DatalinkExceptionHandler.class);
-    
-    @ExceptionHandler(AccessException.class)
-    private ResponseEntity<String> handleAccessException(AccessException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getErrMsg());
-    }
-    
-    @ExceptionHandler(IllegalArgumentException.class)
-    private ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ExceptionUtil.getAllExceptionMsg(e));
-    }
-    
+
     @ExceptionHandler(Exception.class)
-    private ResponseEntity<String> handleException(Exception e) {
-        LOGGER.error("WEB", e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ExceptionUtil.getAllExceptionMsg(e));
+    @ResponseBody
+    private RestResult<Object> handleException(Exception e) {
+        Loggers.WEB.error(e.getMessage());
+        return RestResultUtils.failedWithMsg(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
     }
 }
